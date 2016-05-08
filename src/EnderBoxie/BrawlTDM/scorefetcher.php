c230f092bae4121d101c5f3ce8a718a95ab0d116
Epicrafter60 <sanhancai@gmail.com>
2016-05-08 10:13:09 +0800

    Create scorefetcher.php

---
+++ src/EnderBoxie/BrawlTDM/scorefetcher.php
@@ -0,0 +1,23 @@
+<?php
+namespace BrawlTDM\tasks;
+use pocketmine\scheduler\PluginTask;
+use pocketmine\plugin\Plugin;
+use pocketmine\Player;
+use BrawlTDM\Main;
+class scorefetcher extends PluginTask{
+private $plugin;
+	
+	public function __construct(Main $plugin){
+		parent::__construct($plugin);
+		$this->plugin = $plugin;
+		$this->settings = $this->plugin->settings;
+		$this->map = $this->plugin->map;
+		$this->areans = $this->plugin->areans;
+	}
+	public function onRun($tick){
+	foreach($this->areans->get("Maps") as $m){
+	$this->plugin->checkScore($m);	
+	}
+	$this->plugin->runMatches();
+}
+}
